fd7e3d4a49b0f5a9adb0507d590cff07cd6b9ab8
cv5587 <cv5587@naver.com>
2025-02-25 17:52:48 +0900

    수정

--- CodeTest/psum_ex.cpp
+++ CodeTest/psum_ex.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
+	string dopa = "umzunsik";
+
+	string answer1,answer2,answer3;
+
+	string answer1 = dopa.substr(0, 3);
+	cout << answer1 << "\n";
+
+	for (int i = dopa.length(); i >= 0; i--)
+	{
+		answer2 += dopa[i];
+	}
+
+	answer3 =dopa+ answer2;
+	cout << answer1 << "\n";
+	cout << answer2 << "\n";
+	cout << answer3 << "\n";
 }
