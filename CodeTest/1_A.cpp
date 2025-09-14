86926fd71d7d8334b361080365a52fad524f0d50
Kang <cv5587@naver.com>
2025-09-14 16:12:32 +0900

    헤더 수정및 2309 문제 풀이 중
    체큰

--- CodeTest/1_A.cpp
+++ CodeTest/1_A.cpp
@@ -1,9 +1,34 @@
-#include "bits/stdc++.h"
-
+#include "header.h"
 using namespace std;
 
-
-main()
+void swap(int& a, int& b)
 {
+	int temp = a;
+	a = b;
+	b = temp;
+}
 
+void main()
+{
+	int height[8] = { 0, };
+	for (int i = 0; i < 8; i++) {
+		cin >> height[i];
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = i+1; j < 8; j++)
+		{
+
+			if (height[i]>height[j])
+			{
+				swap(height[i], height[j]);
+			}
+		}
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		cout << height[i] << " ";
+	}
 }
